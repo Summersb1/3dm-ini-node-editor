4d5823e20d739318481b4eef0588821bfeb52105
Summersb1 <somersbyappl@gmail.com>
2024-10-04 09:57:11 +0900

    Initial setup for using BaklavaJS

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div style="width: 100vw; height: 100vh; display: flex; justify-content: center; align-items: center;">
+    <BaklavaEditor :view-model="baklava">
+    </BaklavaEditor>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup lang="ts">
+import { BaklavaEditor, useBaklava, DependencyEngine } from "baklavajs";
+import "@baklavajs/themes/dist/syrup-dark.css";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+// Initialize Baklava editor and dependency engine
+const baklava = useBaklava();
+const MyEditor = baklava.editor;
+const engine = new DependencyEngine(MyEditor);
+engine.start(); // Start the dependency engine
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+// Configuration settings for the Baklava editor
+baklava.settings.nodes.resizable = true;
+baklava.settings.enableMinimap = true;
+baklava.settings.nodes.maxWidth = 800;
+baklava.displayedGraph.scaling *= 0.85;
+//baklava.settings.palette.enabled = false;
+baklava.settings.toolbar.enabled = false;
+</script>
